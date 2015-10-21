8f426b48639d9f68a8bf3c4221f6136f215157eb
somename <someemail>
2015-10-21 15:42:31 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -8,7 +8,7 @@ import org.unioulu.tol.sqat2015.planetExplorer.PlanetExplorer;
 public class TestPlanetExplorer {
 
 	@Test
-	public void testGridCreation() {
+	public void testGridCreationWithObstacles() {
 		
 		PlanetExplorer pe = new PlanetExplorer(100,100,"(1,1)(6,6)");
 		
