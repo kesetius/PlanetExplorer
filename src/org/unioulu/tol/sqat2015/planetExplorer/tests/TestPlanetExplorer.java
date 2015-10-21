0aec81b05697109b75cc69a4fc4e5e4225370c25
somename <someemail>
2015-10-21 16:04:48 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -32,6 +32,18 @@ public class TestPlanetExplorer {
 	
 
 	@Test
+	public void testExplorerPosition_00_00() {
+		PlanetExplorer pe = new PlanetExplorer(100,100,"(1,1)(6,6)");
+		
+		int[][] array = new int[100][100];
+		array[0][0] = 1;
+		array[1][1] = 2;
+		array[6][6] = 2;
+		
+		assertArrayEquals(array,pe.getGrid());	
+	}
+	
+	@Test
 	public void executeCommand() {
 		
 		PlanetExplorer pe = new PlanetExplorer(100,100,"");
