e9246257ba1a1f9a4a0171563d053feedd355651
somename <someemail>
2015-10-21 14:49:40 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -16,7 +16,11 @@ public class PlanetExplorer {
 		Example use:
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
 	 */
-	
+		
+		// Initialize the grid
+		this.setGrid(x, y);
+		
+		
 	}
 	
 	
