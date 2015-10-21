c495dbaf346e84edfd0f4ec84d8a7c167d250b9f
somename <someemail>
2015-10-21 15:38:15 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -39,7 +39,7 @@ public class PlanetExplorer {
 		
 		String obs = obstacles.substring(0,obstacles.length()-1);;
 		
-		obs = obs.replace("(", ""); // Output: "1,2)3,4)5,6).."
+		obs = obs.replace("\\(", ""); // Output: "1,2)3,4)5,6).."
 		System.out.println(obs);
 		String[] coords = obs.split("\\)");
 		Pair[] thePairs = new Pair[ coords.length ];
