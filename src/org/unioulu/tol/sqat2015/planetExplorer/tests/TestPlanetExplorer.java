71241dad9b82a06714b4542b91b1d59d639ddb04
somename <someemail>
2015-10-21 16:09:50 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -37,7 +37,7 @@ public class TestPlanetExplorer {
 		PlanetExplorer pe = new PlanetExplorer(100,100,"(1,1)(6,6)");
 		
 		int[][] array = new int[100][100];
-		array[0][1] = 1;
+		array[1][1] = 1;
 		array[1][1] = 2;
 		array[6][6] = 2;
 		
