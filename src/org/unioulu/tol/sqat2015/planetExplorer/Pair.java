b3ca721edf466743a717e0db081bcd02abff613c
somename <someemail>
2015-10-21 15:02:44 +0300

    besouro automatic message

---
+++ src/org/unioulu/tol/sqat2015/planetExplorer/Pair.java
@@ -0,0 +1,22 @@
+package org.unioulu.tol.sqat2015.planetExplorer;
+
+public class Pair {
+
+	private int x;
+	private int y;
+	
+	public void setPair( int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	
+}
